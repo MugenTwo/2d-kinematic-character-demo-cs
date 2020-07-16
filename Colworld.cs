c190619dbfd56d2189991934d987821a83ae008d
MugenTwo <johncarlo.purihin@gmail.com>
2020-07-16 19:44:50 +0200

    Organizing scripts and assets into folder and updating deprecated methods

--- Colworld.cs
+++ Colworld.cs
@@ -5,7 +5,7 @@ public class Colworld : Node2D
 
     public void onPrincessBodyEntered(PhysicsBody2D body)
     {
-        if (body.GetName().Equals("player"))
+        if (body.Name.Equals("player"))
         {
             Label youWin = GetNode("youwin") as Label;
             youWin.Show();
